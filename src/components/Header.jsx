94b8780b3f2e61472888e8bed234dfdf0d852c92
Azka Anasiyya Haris <azkaanasiyya@gmail.com>
2025-02-28 10:52:14 +0700

    style: update header by replacing text with logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
 import PropTypes from 'prop-types';
+import logo from '../assets/logo header.svg';
 
 export default function Header({ searchTerm, setSearchTerm }) {
   return (
-    <div className="flex max-w-[1200px] mx-auto items-center justify-between p-3 md:p-10">
-      <h1 className="text-2xl font-bold text-neutral-500 md:text-4xl">Pokédex</h1>
-      <input
-        type="text"
-        placeholder="Search Pokémon..."
-        className="rounded-lg border p-2 text-sm"
-        value={searchTerm}
-        onChange={(e) => {
-          setSearchTerm(e.target.value);
-        }}
-      />
+    <div className="mx-auto flex max-w-[1200px] flex-col items-center justify-between gap-7 p-5 md:p-10 md:flex-row md:gap-0">
+      <img src={logo} alt="logo header" className="h-[73.36px] w-52" />
+      <div className="flex items-center gap-2">
+        <input
+          type="text"
+          placeholder="Search Pokémon"
+          className="rounded-full border-4 border-yellow-700 bg-orange-50 text-sm"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+      </div>
     </div>
   );
 }
