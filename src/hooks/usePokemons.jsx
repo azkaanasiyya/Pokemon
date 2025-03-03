ae9ea05e685b2d280643ca8a1718b0db31a3bd53
Azka Anasiyya Haris <azkaanasiyya@gmail.com>
2025-03-03 14:40:59 +0700

    feat(ui): add loading spinner using ClipLoader

--- src/hooks/usePokemons.jsx
+++ src/hooks/usePokemons.jsx
@@ -3,10 +3,11 @@ import { useState, useEffect } from 'react';
 import { getPokemonList } from '../services/Api';
 
 export default function usePokemons() {
-  const [allPokemons, setAllPokemons] = useState([]); 
-  const [filteredPokemons, setFilteredPokemons] = useState([]); 
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [filteredPokemons, setFilteredPokemons] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true); 
   const limit = 12;
 
   useEffect(() => {
@@ -18,14 +19,17 @@ export default function usePokemons() {
   }, [searchTerm, allPokemons]);
 
   const fetchPokemons = async () => {
+    setLoading(true); 
     try {
-      const { results = [] } = await getPokemonList(500, 0); 
+      const { results = [] } = await getPokemonList(500, 0);
       setAllPokemons(results);
       setFilteredPokemons(results);
     } catch (error) {
       console.error("Error fetching Pokémon:", error);
       setAllPokemons([]);
       setFilteredPokemons([]);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -40,5 +44,5 @@ export default function usePokemons() {
   const totalPages = Math.ceil((filteredPokemons?.length || 0) / limit);
   const displayedPokemons = filteredPokemons.slice((page - 1) * limit, page * limit);
 
-  return { pokemons: displayedPokemons, searchTerm, setSearchTerm, page, setPage, totalPages };
+  return { pokemons: displayedPokemons, searchTerm, setSearchTerm, page, setPage, totalPages, loading }; 
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,18 @@ import Header from '../../components/Header';
 import Pagination from '../../components/Pagination';
 import PokemonList from './PokemonList';
 import usePokemons from '../../hooks/usePokemons';
+import { ClipLoader } from 'react-spinners';
 
 export default function Home() {
-  const { pokemons, searchTerm, setSearchTerm, page, setPage, totalPages } = usePokemons();
+  const { pokemons, searchTerm, setSearchTerm, page, setPage, totalPages, loading } = usePokemons();
+
+  if (loading) {
+    return (
+      <div className='flex justify-center items-center h-screen bg-orange-50'>
+        <ClipLoader color="#a16207" size={50} />
+      </div>
+    );
+  }
   return (
     <div className="min-h-screen bg-orange-50">
       <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
